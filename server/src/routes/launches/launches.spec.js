1c91f3df62ce1a2263b5b3c7344cb7f16619ce2c
Rick Phillips <rickwphillips@gmail.com>
2023-02-01 17:15:29 -0500

    Launch API Tests + Added launches test case outlines

---
+++ server/src/routes/launches/launches.spec.js
@@ -0,0 +1,12 @@
+describe('Test GET /launches', function () {
+  test('It should response with 200 success', () => {
+    const response = 200;
+    expect(response).toBe(200);
+  })
+});
+
+describe('Test POST /launches', () => {
+  test('It should response with 200 success', () => {});
+  test('It should catch missing required property', () => {});
+  test('It should catch invalid dates', () => {});
+})
