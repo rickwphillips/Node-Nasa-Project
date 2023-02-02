12e4f8970572357afb92ebfaa227f125177d2482
Rick Phillips <rickwphillips@gmail.com>
2023-02-02 13:23:33 -0500

    Launch API Tests + Added GET & POST tests

--- server/src/routes/launches/launches.spec.js
+++ server/src/routes/launches/launches.spec.js
@@ -1,12 +1,40 @@
+const request = require('supertest');
+const app = require('../../app');
+
 describe('Test GET /launches', function () {
-  test('It should response with 200 success', () => {
-    const response = 200;
-    expect(response).toBe(200);
+  test('It should response with 200 success', async () => {
+    const response = await request(app)
+      .get('/launches')
+      .expect('Content-Type', /json/)
+      .expect(200);
   })
 });
 
 describe('Test POST /launches', () => {
-  test('It should response with 200 success', () => {});
+  test('It should response with 201 created', async () => {
+    const testLaunchData = {
+      mission: 'USS Ricks Starship',
+      rocket: 'RWP 66888-46',
+      target: 'Kepler-186 f',
+    }
+
+    const launchDate = 'January 13, 2076';
+
+    const response = await request(app)
+      .post('/launches')
+      .send({
+        ...testLaunchData,
+        launchDate
+      })
+      .expect('Content-Type', /json/)
+      .expect(201);
+
+    const requestDate = new Date(launchDate).valueOf();
+    const responseDate = new Date(response.body.launchDate).valueOf();
+
+    expect(responseDate).toBe(requestDate);
+    expect(response.body).toMatchObject(testLaunchData);
+  });
   test('It should catch missing required property', () => {});
   test('It should catch invalid dates', () => {});
 })
